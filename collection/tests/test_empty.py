4c8549bba08651f8b9dbb98eda84194554ed5ab2
Robert Harris <30379153+enrh@users.noreply.github.com>
2025-09-13 00:36:01 +0000

    Fix for new linter warnings

---
+++ collection/tests/test_empty.py
@@ -0,0 +1,13 @@
+"""
+Functions and routines associated with Enasis Network Orchestrations.
+
+This file is part of Enasis Network software eco-system. Distribution
+is permitted, for more information consult the project license file.
+"""
+
+
+
+def test_empty() -> None:
+    """
+    Perform various tests associated with relevant routines.
+    """
